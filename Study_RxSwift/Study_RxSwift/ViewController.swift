8679fb4a3b9445211b721523436edce432086027
randychoi95 <jhc130833@naver.com>
2021-03-22 16:14:09 +0900

    first commit

---
+++ Study_RxSwift/Study_RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Study_RxSwift
+//
+//  Created by 최제환 on 2021/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Study_RxSwift/Study_RxSwiftTests/Study_RxSwiftTests.swift
@@ -0,0 +1,33 @@
+//
+//  Study_RxSwiftTests.swift
+//  Study_RxSwiftTests
+//
+//  Created by 최제환 on 2021/03/22.
+//
+
+import XCTest
+@testable import Study_RxSwift
+
+class Study_RxSwiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
